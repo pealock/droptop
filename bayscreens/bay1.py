216ed36a9fbbfa1a73dcfba3e7c0a62ca9b22acb
pealock <andrewpealock@raiden.local>
2024-04-02 22:48:48 -0400

    replaced time with proper WebDriver wait sequences created bayIDs to simply code readability

--- bayscreens/bay1.py
+++ bayscreens/bay1.py
@@ -1,12 +1,14 @@
 import os
-import time
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from dotenv import load_dotenv
+from bayID import bay1, bay2, bay3, bay4
 
 load_dotenv()
 emailLogin = os.getenv("EMAIL")
@@ -19,37 +21,33 @@ options.add_argument("--kiosk")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
                           options=options)
 
+wait = WebDriverWait(driver, 10)
+
 driver.get("https://droptop-app.com")
 
 driver.find_element("xpath", "//input[@type='email']").send_keys(emailLogin)
 driver.find_element("xpath", "//input[@type='password']").send_keys(passwordLogin)
 driver.find_element("xpath", "//input[@type='submit']").click()
 
-time.sleep(3)
+wait.until(EC.presence_of_element_located((By.XPATH, "//div/select")))
 
 siteSelect = Select(driver.find_element("xpath","//div/select"))
 siteSelect.select_by_index(1)
 
-time.sleep(3)
-
 driver.get("https://droptop-app.com/dash/active_orders?t=fullscreen")
 
-time.sleep(3)
-
-
-#Force Bayselect Window
+wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div[4]/div[2]/div[4]/i")))
 
 baySelectWindow = driver.find_element("xpath", "/html/body/div[1]/div/div[4]/div[2]/div[4]/i")
 driver.execute_script("arguments[0].click();", baySelectWindow)
-time.sleep(1)
 
-baySelector1 = driver.find_element("xpath", "/html/body/div/div/div[4]/div[2]/div[7]/div/div[2]/div[1]/div[3]/div/label")
+wait.until(EC.presence_of_element_located((By.XPATH, bay1)))
+
+baySelector1 = driver.find_element("xpath", bay2)
 driver.execute_script("arguments[0].click();", baySelector1)
-baySelector2 = driver.find_element("xpath", "/html/body/div/div/div[4]/div[2]/div[7]/div/div[2]/div[1]/div[4]/div/label")
+baySelector2 = driver.find_element("xpath", bay3)
 driver.execute_script("arguments[0].click();", baySelector2)
-baySelector3 = driver.find_element("xpath", "/html/body/div/div/div[4]/div[2]/div[7]/div/div[2]/div[1]/div[5]/div/label")
+baySelector3 = driver.find_element("xpath", bay4)
 driver.execute_script("arguments[0].click();", baySelector3)
-
-time.sleep(1)
 
 driver.find_element("xpath", "/html/body/div[1]/div/div[4]/div[2]/div[7]/div/div[2]/div[1]/div[6]/button").click()
---
+++ bayscreens/bayID.py
@@ -0,0 +1,7 @@
+bay1 = "/html/body/div/div/div[4]/div[2]/div[7]/div/div[2]/div[1]/div[2]/div/label"
+
+bay2 = "/html/body/div/div/div[4]/div[2]/div[7]/div/div[2]/div[1]/div[3]/div/label"
+
+bay3 = "/html/body/div/div/div[4]/div[2]/div[7]/div/div[2]/div[1]/div[4]/div/label"
+
+bay4 = "/html/body/div/div/div[4]/div[2]/div[7]/div/div[2]/div[1]/div[5]/div/label"
