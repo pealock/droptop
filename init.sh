961ecaa3e378d7d57b9c90f98718842607ce0807
pealock <andrewpealock@raiden.local>
2024-05-16 22:39:09 -0400

    updated init script

--- init.sh
+++ init.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+echo "Droptop Dashboards V1.0 by AMP Studios"
+
+# Set up Python environment
+echo "Setting up Python environment"
+python -m venv ~/.py
+~/.py/bin/pip install selenium python-dotenv
+
 # Set .env file
-echo -e "EMAIL=development@ampstudios.io\nPASSWORD=Ka5hmiro123" > ~/droptop/.env
+echo "Please enter Droptop credentials"
+read -p "Email:" email
+read -p "Password:" password
+echo -e "EMAIL='$email'\nPASSWORD='$password'" > ~/droptop/.env
 
 # Set bg
+echo "Setting background"
 DISPLAY=:0 pcmanfm --set-wallpaper ~/droptop/images/"$HOSTNAME".jpg
 
-# Set auto launch
+# Test launch
+echo "Testing dashboard"
+~/.py/bin/python3 ~/droptop/scripts/bayDashboard.py
