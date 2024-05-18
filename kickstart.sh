546a39d7d617a58879a832eb9e4ac63ea20af23b
pealock <andrewpealock@raiden.local>
2024-05-17 21:38:04 -0400

    more optimizations to kickstart

--- kickstart.sh
+++ kickstart.sh
@@ -7,28 +7,42 @@ echo
 echo
 echo
 
+sleep 3
+
 # Git clone
 echo "Cloning git repository into home directory"
+echo
+echo
+echo
 git clone https://github.com/pealock/droptop.git ~/droptop
 echo
 echo
 echo
 
+sleep 3
+
 # Set up Python environment
 echo "Checking for Python environment"
 echo
 echo
+echo
 PYTHON=~/.py
 if test -d "$PYTHON"; then
-  echo "Python environment already configured"
+  echo "Python environment already configured!"
 else
   echo "Setting up Python environment"
   python -m venv ~/.py
   ~/.py/bin/pip install selenium python-dotenv
+  echo
+  echo
+  echo
+  echo "Python environment configured"
 fi
 echo
 echo
 echo
+
+sleep 3
 
 # Check for .env file, create if none exists
 echo "Checking for credentials"
@@ -49,6 +63,7 @@ else
   echo
 fi
 
+sleep 3
 
 # Set bg
 echo "Setting background"
@@ -57,6 +72,8 @@ echo
 echo
 echo
 
+sleep 3
+
 # Test launch
 echo "Testing dashboard"
 DISPLAY=:0 ~/.py/bin/python3 ~/droptop/scripts/bayDashboard.py
@@ -64,6 +81,8 @@ killall chromium-browser
 echo
 echo
 echo
+
+sleep 3
 
 echo "Setting up boot environment"
 echo "DISPLAY=:0 ~/.py/bin/python3 ~/droptop/scripts/bayDashboard.py" >> ~/.bashrc
