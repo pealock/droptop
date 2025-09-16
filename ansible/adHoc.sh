5a42a188a6cab9ed337f31b9091826de60429ca7
pealock <andrewpealock@gmail.com>
2025-09-16 10:42:29 -0400

    updated adhoc scripts

--- ansible/adHoc.sh
+++ ansible/adHoc.sh
@@ -2,7 +2,7 @@
 ansible all -m ping
 
 # Git pull
-ansible all -m shell -a "~/droptop && git pull"
+ansible all -m shell -a "cd ~/droptop && git pull"
 
 # Run dashboard command
 ansible all -m shell -a "DISPLAY=:0 ~/.py/bin/python3 ~/droptop/scripts/bayDashboard.py"
