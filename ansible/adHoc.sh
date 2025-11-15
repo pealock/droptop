84c78c032aed53bfff93c950edd89f2f7421ad11
pealock <andrewpealock@gmail.com>
2025-11-15 08:54:15 -0500

    updated adhoc scripts

--- ansible/adHoc.sh
+++ ansible/adHoc.sh
@@ -6,3 +6,6 @@ ansible all -m shell -a "cd ~/droptop && git pull"
 
 # Run dashboard command
 ansible all -m shell -a "DISPLAY=:0 ~/.py/bin/python3 ~/droptop/scripts/bayDashboard.py"
+
+# Reboot
+ansible all -m shell -a "sudo reboot"
