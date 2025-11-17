108803e19f55c83002b9371090e92b0929d541c3
pealock <andrewpealock@gmail.com>
2025-11-17 12:30:19 -0500

    11.17.2025 bay select updates - had to refactor the xpath selector to use span explicitly. recent updated added an input wrapper with "Bay #" so the previously working wildcard asterisk selector was invalid

--- scripts/bayID.py
+++ scripts/bayID.py
@@ -1,7 +1,7 @@
-bay1 = "//*[contains(text(), 'Bay 1')]"
+bay1 = "//span[contains(text(), 'Bay 1')]"
 
-bay2 = "//*[contains(text(), 'Bay 2')]"
+bay2 = "//span[contains(text(), 'Bay 2')]"
 
-bay3 = "//*[contains(text(), 'Bay 3')]"
+bay3 = "//span[contains(text(), 'Bay 3')]"
 
-bay4 = "//*[contains(text(), 'Bay 4')]"
+bay4 = "//span[contains(text(), 'Bay 4')]"
